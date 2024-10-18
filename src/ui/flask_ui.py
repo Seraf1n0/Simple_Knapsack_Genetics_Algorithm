5344b8d10905e35e8af6b498911b7eb95fbfffbc
Dylan Rodríguez <dylrodriguez@estudiantec.cr>
2024-10-18 00:16:54 -0600

    Lista la primera parte de la interfaz. Falta hacer que se llene la tabla

--- src/ui/flask_ui.py
+++ src/ui/flask_ui.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('main.html', respuesta=True)
 
 
 if __name__ == '__main__':
